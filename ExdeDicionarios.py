382388b1cfe02832f716b6ec30c6cb8c6bcf63e3
Cauê Iwamoto Meira <92590617+CaueIwamoto@users.noreply.github.com>
2021-11-03 21:48:35 -0300

    Update ExdeDicionarios.py

--- ExdeDicionarios.py
+++ ExdeDicionarios.py
@@ -1,5 +1,14 @@
 #Exercício 1:
+def converte(dicionario, frase):
+    frase = frase.upper()
+    frase_morse = []
 
+    for i in frase:
+        if i in dicionario:
+            frase_morse.append(dicionario[i] + " ")
+
+    return "".join(frase_morse)
+ 
 #Exercício 2:
 
 #Exercício 3:
